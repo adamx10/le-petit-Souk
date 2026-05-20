83a501bb15e9181cbba504456b61e2d7c6036a4a
adam <hazimaa1254@gmail.com>
2026-05-20 02:43:51 +0100

    first

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import {  View } from "react-native";
+import  Header from "../assets/components/header";
+
+
+export default function Index() {
+
+  return (
+    
+
+    <View style={{ backgroundColor:"#DFC560", flex :1,}}>
+<Header />
+  
+    </View>
+  );
+}
